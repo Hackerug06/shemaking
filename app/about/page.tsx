46a7ef812c1c80f8b2e65d04970be10874baab40
Hackerug06 <hackerug06@gmail.com>
2025-01-24 14:40:55 +0300

    Update page.tsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,5 +1,5 @@
-import { AnimatedHeading } from "@/components/animated-heading"
-import { ArtistProfile } from "@/components/artist-profile"
+import { AnimatedHeading } from "@/components/ui/animated-heading"
+import { ArtistProfile } from "@/components/ui/artist-profile"
 
 export default function About() {
   return (
@@ -11,3 +11,4 @@ export default function About() {
   )
 }
 
+      
