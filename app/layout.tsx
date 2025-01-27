3b0589648c6d1b76275fa016876e0f4d48e6314d
Hackerug06 <hackerug06@gmail.com>
2025-01-27 15:36:44 +0300

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from "next"
+import { Inter } from "next/font/google"
+import "./globals.css"
+import Navbar from "./components/Navbar"
+
+const inter = Inter({ subsets: ["latin"] })
+
+export const metadata: Metadata = {
+  title: "Shema King👑",
+  description: "Official website of artist Shema King👑",
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} bg-gray-900 text-white`}>
+        <Navbar />
+        {children}
+      </body>
+    </html>
+  )
+}
+
+  
