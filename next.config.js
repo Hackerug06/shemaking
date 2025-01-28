553da4ea828ef67b7bdb2fa09d21296f5ca05097
Hackerug06 <hackerug06@gmail.com>
2025-01-28 15:14:04 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+  output: "standalone",
   images: {
     unoptimized: true,
+  },
+  // Ensure static files are copied to the correct location
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(mp3)$/,
+      type: "asset/resource",
+      generator: {
+        filename: "static/media/[name][ext]",
+      },
+    })
+    return config
   },
 }
 
 module.exports = nextConfig
 
-    
+      
