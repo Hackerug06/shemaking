f922c158e4ed58ebe149a9a63d0bbfa995a4f622
Hackerug06 <hackerug06@gmail.com>
2025-01-29 23:50:54 +0300

    Update page.tsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -27,21 +27,33 @@ export default async function About() {
         </div>
         <div className="md:w-1/2">
           <p className="text-lg mb-4">
-            Shema King👑 is a rising star in the music industry, known for his unique blend of afrobeats and
-            contemporary R&B. Born and raised in Kampala, Uganda, Shema discovered his passion for music at a young age.
-          </p>
-          <p className="text-lg mb-4">
-            With a voice that captivates and lyrics that inspire, Shema King👑 has been making waves in the music scene
-            since his debut in 2023. His music often explores themes of love, empowerment, and cultural pride.
-          </p>
+            <p className="text-lg mb-4">
+            <b>Uwimbabazi Shema</b> known as Shema King is a rising star in the music industry. He was
+              born on <b>12th December 2007</b>. He was born and raised in Kampala, Uganda.He has a voice 
+              that captivates and lyrics that inspire. Heis widely
+              recognised by his stage name <b>"Shema King"</b>.
+              He is an African rapper who lives in Uganda since his debut in 2023.
+              His music often explores themes of love, empower, and cultural pride.
+              He does multiple generals of music such as drill,
+              hiphop, afrobeat ,afropop and amapiano music. This makes him
+              to be multi-talented.
+            </p>
+          
           <p className="text-lg">
             Shema King👑's journey is just beginning, and he's excited to share his music with the world. Stay tuned for
             upcoming releases and live performances!
           </p>
+            <h1 className="text-4xl font-bold mb-6 animate-text bg-gradient-to-r from-teal-500 via-purple-500 to-orange-500 bg-clip-text text-transparent">
+        Shema King👑 Motto
+      </h1>
+            <p className="text-lg">
+            It's time for the best, No time for the rest #shemaking </p>
+      
         </div>
       </div>
     </main>
   )
 }
+
 
       
